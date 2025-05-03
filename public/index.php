2da51d33bfbb8dd65ac7e063d602674fa8e9357d
Eazcee <143572290+Eazcee@users.noreply.github.com>
2025-05-02 21:53:54 -0400

    updated by adding styles and centering image using margin auto

--- public/index.php
+++ public/index.php
@@ -1,24 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-    <h1 style="color:blue;">Capybara Lovesite<br></h1>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <h1><i>Capybara Lovesite</i><br></h1>
 
-    <body style="background-color:powderblue;">
+        <div>
+            <a href="https://www.google.com/search?client=opera-gx&hs=bz5&sca_esv=032488618a09524f&sxsrf=AHTn8zoMjw2fuwfPoli6wdIF7HKpzyVFIQ:1746234054578&q=capybara+picture&udm=2&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBsxayPSIAqObp_AgjkUGqemmTG2DFZE7tmKcXVp5H8R-R5rx2cv4LdpyiJ4nHnaj22nzwRLb038fZUcDagzuU5JMvWvtjGNLL7KXZuMGJ8jJcUt7C8AOlnuhFVueytR4tMw9VPc1DLuELRcJx-63pH51AnlFH8SKHOwWLbFKS1gU3p3P7g&sa=X&ved=2ahUKEwjW_pHkjIaNAxXtTDABHYwUAfQQtKgLegQIHBAB&biw=695&bih=764&dpr=2" target="_blank">
+            <img src="capybara.jpg" width='250' height = "250">
+            </a>
+        </div>
+        <br>
+        <hr>
+        <h2>
+            Why Capybaras?
+        </h2>
+        <p>
+            Every since their first introduction into the ecosystem, they have shown their <em>strength</em> to being chill.
+        </p>
+            <hr>
+        <h2>
+            Where it all started:
+        </h2>
+        <p>
+            These lovable creatures come from South America: <q>specifically the wetlands and forested areas near bodies of water.</q>
+        </p>
+        <hr>
+        <h2> 
+            A small poem for Capybaras:
+            </h2>
+        <pre>
+            My Bonnie lies over the ocean.
 
-    <img src="capybara.jpg" width='250' height = "250"><br>
-    <hr>
-    <h1 style="color:blue">Why Capybaras?</h1>
-    <p>Every since their first introduction into the ecosystem, they have shown their resilliance to being chill.</p>
-    <hr>
-    <h2 style="color:blue">Where it all started</h2>
-    <p>These lovable creatures come from South America, specifically the wetlands and forested areas near bodies of water.</p>
-    <hr>
-    <h2 style="color:blue"> A small poem for Capybaras</h2>
-    <pre>
-    My Bonnie lies over the ocean.
+            My Bonnie lies over the sea.
 
-    My Bonnie lies over the sea.
+            My Bonnie lies over the ocean.
 
-    My Bonnie lies over the ocean.
-
-    Oh, bring back my Bonnie to me.
-    </pre>
+            Oh, bring back my Bonnie to me.
+        </pre>
